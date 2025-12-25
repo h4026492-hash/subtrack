480457e94a783452a9bdb9b2eb4a432824d52205
Harshith Battu <h4026492@gmail.com>
2025-12-25 09:32:51 -0600

    fix(ui): clean RotatingCube to canonical reanimated implementation

--- components/RotatingCube.tsx
+++ components/RotatingCube.tsx
@@ -4,6 +4,7 @@ import Animated, {
   useSharedValue,
   useAnimatedStyle,
   withTiming,
+  withRepeat,
 } from 'react-native-reanimated'
 import { LinearGradient } from 'expo-linear-gradient'
 
@@ -13,78 +14,164 @@ export default function RotatingCube() {
   const rotation = useSharedValue(0)
 
   useEffect(() => {
-    rotation.value = Animated.withRepeat(
+    rotation.value = withRepeat(
       withTiming(360, { duration: 9000 }),
       -1,
       false
     )
   }, [])
 
-  const animatedStyle = useAnimatedStyle(() => ({
+  const cubeStyle = useAnimatedStyle(() => ({
     transform: [
-      { perspective: 800 },
-      { rotateX: `${rotation.value}deg` },
       { rotateY: `${rotation.value}deg` },
+      { rotateX: `${rotation.value * 0.7}deg` },
     ],
   }))
 
   return (
-    <Animated.View style={[styles.container, animatedStyle]}>
-      <Face colors={['#6A5BFF', '#3CE7F6']} />
-      <Face colors={['#3CE7F6', '#9B8CFF']} style={styles.back} />
-      <Face colors={['#5F6CFF', '#3CE7F6']} style={styles.left} />
-      <Face colors={['#9B8CFF', '#6A5BFF']} style={styles.right} />
-      <Face colors={['#6A5BFF', '#3CE7F6']} style={styles.top} />
-      <Face colors={['#3CE7F6', '#6A5BFF']} style={styles.bottom} />
+    <Animated.View style={[styles.cube, cubeStyle]}>
+      <LinearGradient
+        colors={['#6a5cff', '#3bdcff']}
+        style={styles.face}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+      />
     </Animated.View>
   )
 }
 
-function Face({ colors, style }: any) {
+const styles = StyleSheet.create({
+  cube: {
+    width: SIZE,
+    height: SIZE,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  face: {
+    width: SIZE,
+    height: SIZE,
+    borderRadius: 24,
+  },
+})
+import React, { useEffect } from 'react'
+import { StyleSheet } from 'react-native'
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withRepeat,
+} from 'react-native-reanimated'
+import { LinearGradient } from 'expo-linear-gradient'
+
+const SIZE = 120
+
+export default function RotatingCube() {
+  const rotation = useSharedValue(0)
+
+  useEffect(() => {
+    rotation.value = withRepeat(
+      withTiming(360, { duration: 9000 }),
+      -1,
+      false
+    )
+  }, [])
+
+  const cubeStyle = useAnimatedStyle(() => ({
+    transform: [
+      { rotateY: `${rotation.value}deg` },
+      { rotateX: `${rotation.value * 0.7}deg` },
+    ],
+  }))
+
+  return (
+    <Animated.View style={[styles.cube, cubeStyle]}>
+      <LinearGradient
+        colors={['#6a5cff', '#3bdcff']}
+        style={styles.face}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+      />
+    </Animated.View>
+  )
+}
+
+const styles = StyleSheet.create({
+  cube: {
+    width: SIZE,
+    height: SIZE,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  face: {
+    width: SIZE,
+    height: SIZE,
+    borderRadius: 24,
+  },
+})
+import React, { useEffect } from 'react'
+import { StyleSheet } from 'react-native'
+import React, { useEffect } from 'react'
+import { View, StyleSheet } from 'react-native'
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withRepeat,
+  interpolate,
+} from 'react-native-reanimated'
+import { LinearGradient } from 'expo-linear-gradient'
+
+const SIZE = 120
+
+export default function RotatingCube() {
+  const rotation = useSharedValue(0)
+
+  useEffect(() => {
+    rotation.value = withRepeat(
+      withTiming(360, { duration: 9000 }),
+      -1,
+      false
+    )
+  }, [])
+
+  const cubeStyle = useAnimatedStyle(() => ({
+    transform: [
+      { rotateY: `${rotation.value}deg` },
+      { rotateX: `${rotation.value * 0.7}deg` },
+    ],
+  }))
+
+  return (
+    <Animated.View style={[styles.cube, cubeStyle]}>
+      <Face colors={['#6a5cff', '#3bdcff']} />
+    </Animated.View>
+  )
+}
+
+function Face({ colors }: { colors: string[] }) {
   return (
     <LinearGradient
       colors={colors}
+      style={styles.face}
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 1 }}
-      style={[styles.face, style]}
     />
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  cube: {
     width: SIZE,
     height: SIZE,
-    alignSelf: 'center',
-    marginBottom: 36,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-
   face: {
-    position: 'absolute',
     width: SIZE,
     height: SIZE,
-    borderRadius: 20,
-    opacity: 0.9,
-  },
-
-  back: {
-    transform: [{ rotateY: '180deg' }],
-  },
-
-  left: {
-    transform: [{ rotateY: '-90deg' }],
-  },
-
-  right: {
-    transform: [{ rotateY: '90deg' }],
-  },
-
-  top: {
-    transform: [{ rotateX: '90deg' }],
-  },
-
-  bottom: {
-    transform: [{ rotateX: '-90deg' }],
+    borderRadius: 24,
   },
 })
+    height: SIZE,
 
+    borderRadius: 20,
