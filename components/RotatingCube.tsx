b6518cb392d4ff826ff032b6291dd37af75fb548
Harshith Battu <h4026492@gmail.com>
2025-12-26 11:52:46 -0600

    feat(ui): final RotatingCube implementation (6 faces, perspective, backface visibility)

--- components/RotatingCube.tsx
+++ components/RotatingCube.tsx
@@ -1,9 +1,82 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useEffect } from 'react'
+import { View, StyleSheet, Image } from 'react-native'
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withRepeat,
+} from 'react-native-reanimated'
 
-// Deprecated: kept as a small stub while `RotatingCubeFixed` is used in the app.
+const SIZE = 120
+const HALF = SIZE / 2
+
 export default function RotatingCube() {
-  return <View />
+  const rotate = useSharedValue(0)
+
+  useEffect(() => {
+    rotate.value = withRepeat(
+      withTiming(360, { duration: 8000 }),
+      -1,
+      false
+    )
+  }, [])
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      { perspective: 900 },
+      { rotateY: `${rotate.value}deg` },
+      { rotateX: `${rotate.value * 0.6}deg` },
+    ],
+  }))
+
+  return (
+    <View style={styles.scene}>
+      <Animated.View style={[styles.cube, animatedStyle]}>
+        <Face logo={require('../assets/logos/netflix.png')} style={{ transform: [{ translateZHack: 1 }] }} />
+        <Face logo={require('../assets/logos/prime.png')} style={{ transform: [{ rotateY: '90deg' }, { translateX: HALF }] }} />
+        <Face logo={require('../assets/logos/hulu.png')} style={{ transform: [{ rotateY: '180deg' }, { translateX: HALF }] }} />
+        <Face logo={require('../assets/logos/appletv.png')} style={{ transform: [{ rotateY: '-90deg' }, { translateX: HALF }] }} />
+        <Face logo={require('../assets/logos/paramount.png')} style={{ transform: [{ rotateX: '90deg' }, { translateY: -HALF }] }} />
+        <Face logo={require('../assets/logos/disney.png')} style={{ transform: [{ rotateX: '-90deg' }, { translateY: HALF }] }} />
+      </Animated.View>
+    </View>
+  )
 }
+
+function Face({ logo, style }: any) {
+  return (
+    <View style={[styles.face, style]}>
+      <Image source={logo} style={styles.logo} resizeMode="contain" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  scene: {
+    width: SIZE,
+    height: SIZE,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cube: {
+    width: SIZE,
+    height: SIZE,
+    position: 'relative',
+  },
+  face: {
+    position: 'absolute',
+    width: SIZE,
+    height: SIZE,
+    backgroundColor: '#0B1020',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    backfaceVisibility: 'hidden',
+  },
+  logo: {
+    width: 64,
+    height: 64,
+  },
+})
 
 
